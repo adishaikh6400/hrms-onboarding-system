4f6c454f0451bfe2d08cb459dc222d233bace911
Adi Shaikh <shaikhadi640@gmail.com>
2026-03-31 22:23:51 +0530

    added lambda folder

--- lib/hrms-onboarding-stack.ts
+++ lib/hrms-onboarding-stack.ts
@@ -1,16 +1,39 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class HrmsOnboardingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // ✅ DynamoDB Table
+    const table = new dynamodb.Table(this, 'EmployeesTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HrmsOnboardingQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ✅ Lambda Function
+    const createEmployeeLambda = new lambda.Function(this, 'CreateEmployeeFn', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // ✅ Permissions
+    table.grantWriteData(createEmployeeLambda);
+
+    // ✅ API Gateway
+    const api = new apigateway.RestApi(this, 'HRMSApi', {
+      restApiName: 'HRMS Service',
+    });
+
+    const employee = api.root.addResource('employee');
+    employee.addMethod('POST', new apigateway.LambdaIntegration(createEmployeeLambda));
   }
 }
