b6774ea7d343047217d34f01a8c101f95bea1453
Adi Shaikh <shaikhadi640@gmail.com>
2026-03-31 20:35:56 +0530

    Initial commit

---
+++ test/hrms-onboarding.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HrmsOnboarding from '../lib/hrms-onboarding-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hrms-onboarding-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HrmsOnboarding.HrmsOnboardingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
